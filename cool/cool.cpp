c8c279d3676f5aed525726976ce36ce69ccbec98
Anton <aantons72@mail.ru>
2024-10-21 11:14:22 +0500

    add return

--- cool/cool.cpp
+++ cool/cool.cpp
@@ -11,6 +11,10 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
+
+
+
+    return 0;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
