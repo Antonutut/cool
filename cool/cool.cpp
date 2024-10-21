a6d1ce942b78b82be81b174805c49b73cc85fc5e
Antonutut <aantosha228@gmail.com>
2024-10-21 11:26:02 +0500

    Update cool.cpp

--- cool/cool.cpp
+++ cool/cool.cpp
@@ -2,16 +2,16 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    
 
 
     return 0;
